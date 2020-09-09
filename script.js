b8eea5be03d6004649f889c1d6abbc41c88a584d
Erin Crocker <erindowns1@me.com>
2020-09-08 21:46:57 -0400

    set up the html document for index.html and highscore.html with some styling in css document

---
+++ script.js
@@ -0,0 +1,21 @@
+var startQuizButton = document.querySelector("#start-quiz");
+console.log(startQuizButton)
+var countdownTimer = document.querySelector("#timer");
+var startQuizPage = document.querySelector("#start-quiz-page")
+var questionPage = document.querySelector("#Question-display-page");
+var ScorePage = document.querySelector("#Score-display-page");
+
+
+startQuizButton.addEventListener("click",function(){
+
+    console.log("you clicked this");
+    startQuizPage.style.display = 'none';
+    questionPage.style.display = 'block';
+    // hide start-quiz-page
+    // add question h2
+    // add 4 buttons with answers
+
+
+
+    // add a set interval for timer 
+});
